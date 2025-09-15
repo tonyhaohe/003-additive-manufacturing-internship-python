f3ad3f71b6aa9f5537919642f7c49a0b992e9e94
Tony He <82583238+tonyhaohe@users.noreply.github.com>
2025-09-15 15:13:03 +0100

    Fix rounding-related bug, refactor, allow navigation using arrows

--- grossman_heatmap.py
+++ grossman_heatmap.py
@@ -6,6 +6,101 @@ from collections import deque # This is for adding power labels.
 
 from utilities import new_entry, predict_depth, predict_width, calculate_ellipsoid_volume
 
+fig = plt.figure(figsize = (8,8), layout = "constrained")
+
+def draw(metal): 
+    this_entry = json_data[metal]
+    if "experiment_data" not in this_entry: 
+        print("There is no experimental data, the heatmap for " + metal + " can't generate")
+        return
+    pow_lb = this_entry["pow_lb"] 
+    pow_ub = this_entry["pow_ub"]
+    pow_interval = this_entry["pow_interval"] 
+    pow_num_intervals = 0 
+    while (pow_num_intervals == 0):
+        pow_range = pow_ub - pow_lb 
+        while (pow_range - pow_interval >= 0): 
+            pow_num_intervals += 1 
+            pow_range -= pow_interval
+        if pow_num_intervals == 0: 
+            print("Step size is too large for range, please enter a smaller step")
+            pow_interval = float(input("What is the step size of power?: "))
+    pow_num_intervals += 1 # We include the lower bound graph
+    speed_lb = this_entry["speed_lb"]
+    speed_ub = this_entry["speed_ub"]
+    speed_interval = this_entry["speed_interval"]
+    speed_num_intervals = 0 
+    while (speed_num_intervals == 0):
+        speed_range = speed_ub - speed_lb 
+        while (speed_range - speed_interval >= 0): 
+            speed_num_intervals += 1 
+            speed_range -= speed_interval
+        if speed_num_intervals == 0: 
+            print("Step size is too large for range, please enter a smaller step")
+            speed_interval = float(input("What is the step size of speed?: "))
+    speed_num_intervals += 1
+    
+    percentage_error_matrix = np.ones((pow_num_intervals, speed_num_intervals)) # Creating the numpy array (which is a matrix) to put into the heatmap 
+    highest_percentage_error = np.zeros((1)) # To keep track of the highest percent and then eventually define the scale of the heatmap
+    
+    fig.canvas.manager.set_window_title("Figure 1 - " + metal)
+
+    fig.suptitle(metal + ": The effect of scanning speed (mm$s^{-1}$) & power (W) on melt pool width & depth(μm) with laser diameter " + str(round(this_entry["sigma"]*(10**6),0)) + "μm")
+
+
+    power = deque()
+    speed = []
+    # Two loops so that we go through all the desired values of power and speed and calculate the cross-sectional area of the melt pool 
+    for pow_count in range(0,pow_num_intervals,1):
+        power_level = pow_lb+(pow_count)*pow_interval
+        power.appendleft(str(power_level) + "W") # Creating labels for the vertical axis 
+        for speed_count in range (0,speed_num_intervals,1):
+            speed_level = speed_lb+(speed_count)*speed_interval
+            # Heatmap cell coordinate for this specific combination of power and speed 
+            row = pow_num_intervals-pow_count-1
+            column = speed_count
+
+            ## PREDICTED MELT POOL VOLUME CALCULATION ##
+            # Melt pool width and depth of this specific combination of power and speed
+            pred_width = predict_width(power_level,speed_level,json_data, metal)
+            pred_depth = predict_depth(power_level,speed_level,json_data, metal)
+            predicted_volume = calculate_ellipsoid_volume(pred_width, pred_depth)
+            actual_width = this_entry["experiment_data"][str(power_level)+ ","+str(speed_level)][0]
+            actual_depth = this_entry["experiment_data"][str(power_level)+ ","+str(speed_level)][1]
+            actual_volume = calculate_ellipsoid_volume(actual_width, actual_depth)
+
+            percentage_error = predicted_volume/actual_volume * 100 
+            ## PROCESSING THE PERCENTAGE ERROR BY CHANGING THE SIGN AND BY RE-ASSIGNING WHAT IS THE HIGHEST PERCENTAGE ERROR IF NECESSARY ##
+            if (pow_count<1):
+                speed.append(str(speed_level * (10**3)) + " mms⁻¹") # Creating labels for the horizontal axis. This only needs to be done once, which can be done in the pow_count = 0 iteration
+            if (predicted_volume<actual_volume):
+                percentage_error =-2* percentage_error # If the predicted volume was lower than the actual, this is reflected in the percentage error with the minus sign in front of the percentage
+            if (percentage_error>highest_percentage_error[0]):
+                highest_percentage_error[0] = percentage_error # Likewise, if the predicted volume was actually larger we just keep it in the positives
+            percentage_error_matrix[row][column] = percentage_error - 1e-9 # You need the 1e-9 because it pushes you into the boundary of the cell so the console doesn't start throwing error messages (notice that if you remove this, the bottom left corner of the window for some cells might not even display the value)
+    norm = mcolors.TwoSlopeNorm(vmin=-highest_percentage_error, vcenter = 0, vmax = highest_percentage_error)
+    ax = fig.add_subplot()
+    heatmap(percentage_error_matrix, power , speed, ax=ax,
+                        cmap="bwr", cbarlabel="Percentage error", norm = norm)
+    fig.canvas.draw_idle()
+    
+
+i = 0    
+keys = []
+    
+def on_key(e): 
+    fig.clf()
+    global i
+    global keys
+    if e.key in ("right", "n"):
+        i = (i + 1) % len(keys)
+    elif e.key in ("left", "p"):
+        i = (i - 1) % len(keys)
+    draw(keys[i])
+
+# Connect key press event
+fig.canvas.mpl_connect('key_press_event', on_key)
+
 def heatmap(data, row_labels, col_labels, ax=None,
             cbar_kw=None, cbarlabel="", **kwargs):
     """
@@ -62,63 +157,18 @@ def heatmap(data, row_labels, col_labels, ax=None,
     return im, cbar
 
 if (__name__=='__main__'):
-    percentage_error_matrix = np.ones((4, 3)) # Creating the numpy array (which is a matrix) to put into the heatmap 
-    highest_percentage_error = np.zeros((1)) # To keep track of the highest percent and then eventually define the scale of the heatmap
     option = input("(Enter/anything) To access previous entries | 'n' To create new entries | 'q' To quit: ").lower()
     if (option=="q"):
         quit()
     elif (option=="n"):
         new_entry()
     else: 
-        f= open("data.json","r",encoding="utf-8")
-        json_data = json.load(f)
-        for metal in json_data:
-            this_entry = json_data[metal]
-            pow_lb = this_entry["pow_lb"] 
-            pow_ub = this_entry["pow_ub"]
-            pow_interval = this_entry["pow_interval"] 
-            pow_num_intervals = this_entry["pow_num_intervals"]
-            speed_lb = this_entry["speed_lb"]
-            speed_ub = this_entry["speed_ub"]
-            speed_interval = this_entry["speed_interval"]
-            speed_num_intervals = this_entry["speed_num_intervals"]
-
-            power = deque()
-            speed = []
-            # Two loops so that we go through all the desired values of power and speed and calculate the cross-sectional area of the melt pool 
-            for pow_count in range(0,pow_num_intervals,1):
-                power_level = pow_lb+(pow_count)*pow_interval
-                power.appendleft(str(power_level) + "W") # Creating labels for the vertical axis 
-                for speed_count in range (0,speed_num_intervals,1):
-                    speed_level = speed_lb+(speed_count)*speed_interval
-                    # Heatmap cell coordinate for this specific combination of power and speed 
-                    row = pow_num_intervals-pow_count-1
-                    column = speed_count
-
-                    ## PREDICTED MELT POOL VOLUME CALCULATION ##
-                    # Melt pool width and depth of this specific combination of power and speed
-                    pred_width = predict_width(power_level,speed_level,json_data, metal)
-                    pred_depth = predict_depth(power_level,speed_level,json_data, metal)
-                    predicted_volume = calculate_ellipsoid_volume(pred_width, pred_depth)
-                    actual_width = this_entry["experiment_data"][str(power_level)+ ","+str(speed_level)][0]
-                    actual_depth = this_entry["experiment_data"][str(power_level)+ ","+str(speed_level)][1]
-                    actual_volume = calculate_ellipsoid_volume(actual_width, actual_depth)
-
-                    percentage_error = predicted_volume/actual_volume * 100 
-                    ## PROCESSING THE PERCENTAGE ERROR BY CHANGING THE SIGN AND BY RE-ASSIGNING WHAT IS THE HIGHEST PERCENTAGE ERROR IF NECESSARY ##
-                    if (pow_count<1):
-                        speed.append(str(speed_level * (10**3)) + " mms⁻¹") # Creating labels for the horizontal axis. This only needs to be done once, which can be done in the pow_count = 0 iteration
-                    if (predicted_volume<actual_volume):
-                        percentage_error =-2* percentage_error # If the predicted volume was lower than the actual, this is reflected in the percentage error with the minus sign in front of the percentage
-                    if (percentage_error>highest_percentage_error[0]):
-                        highest_percentage_error[0] = percentage_error # Likewise, if the predicted volume was actually larger we just keep it in the positives
-                    percentage_error_matrix[row][column] = percentage_error 
-            print("Loaded!")
-            fig, ax = plt.subplots(figsize = (8,8), layout = "constrained")
-
-            fig.suptitle(metal + ": The effect of scanning speed (mm$s^{-1}$) & power (W) on melt pool width & depth (μm)")
-            norm = mcolors.TwoSlopeNorm(vmin=-highest_percentage_error, vcenter = 0, vmax = highest_percentage_error)
-            im, cbar = heatmap(percentage_error_matrix, power , speed, ax=ax,
-                            cmap="bwr", cbarlabel="Percentage error", norm = norm)
-            plt.show()
-            quit()
+        print("Use arrow keys ← Left and → Right to navigate entries")
+        json_data = {}
+        with open("data.json","r",encoding="utf-8") as f:
+            json_data = json.load(f)
+        keys = list(json_data)
+        draw(keys[0])
+        plt.show()
+        print("Loaded!")
+        quit()
